12eacbce70b1ad8fd3f45616e1146a7b1130345d
tu nombre <tumail@gmail.com>
2016-11-03 17:17:49 -0500

    first commit

---
+++ auth_web/app/config.js
@@ -0,0 +1,52 @@
+﻿var baseUrl = 'http://localhost:9000/';
+var loginUrl = 'http://localhost:9001/auth_web/';
+var iotecaUrl = 'http://localhost:9001/ventax_web/';
+
+
+var clientId = 'DP80VrtliNTby3SKAvgEuCL4JteCtxLsFnoV9OUa';
+var clientSecret = '558SnvaxyO1jgJ2fW3hvKLf4Myw72qTeOXp3zzSBJJKC407ZuXgNLlxgIEYZLbXhD8hhImT8ESpVnododG6WlSt60XDsWvTPi0I3tNjRKwu67dznV1ZakcgjXZs6E7VN';
+var grantType = 'password';
+
+var config = {
+
+    baseUrl: baseUrl,
+    loginUrl: loginUrl,
+    iotecaUrl: iotecaUrl,
+
+    clientId: clientId,
+    clientSecret: clientSecret,
+    grantType: grantType,
+
+};
+
+app.value('config', config);
+
+app
+    .run(function($rootScope, $state, $stateParams, $window, loginService) {
+        // It's very handy to add references to $state and $stateParams to the $rootScope
+        // so that you can access them from any scope within your applications.For example,
+        // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+        // to active whenever 'contacts.list' or one of its decendents is active.
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+
+        /*******************************agregado**************************/
+        console.log("run");
+
+        if (loginService.authentication.isAuth === false) {
+            $window.location = loginUrl;
+        }
+        /******************************************************************/
+
+    })
+
+.config(function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    //$httpProvider.interceptors.push('authInterceptorService');
+})
+
+.config(function($resourceProvider) {
+    // Don't strip trailing slashes from calculated URLs
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+});
